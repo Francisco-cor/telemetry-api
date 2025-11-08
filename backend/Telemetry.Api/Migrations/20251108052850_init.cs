a798112d77333bcee08d295946b7364f450cd983
Francisco Cordero <128191692+Francisco-cor@users.noreply.github.com>
2025-11-07 23:40:21 -0600

    feat(db): implement ef core model and migrations for telemetry

---
+++ backend/Telemetry.Api/Migrations/20251108052850_init.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Telemetry.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Telemetry",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "RAW(16)", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false),
+                    Source = table.Column<string>(type: "NVARCHAR2(100)", maxLength: 100, nullable: false),
+                    MetricName = table.Column<string>(type: "NVARCHAR2(100)", maxLength: 100, nullable: false),
+                    MetricValue = table.Column<double>(type: "BINARY_DOUBLE", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Telemetry", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Telemetry_Source_Ts",
+                table: "Telemetry",
+                columns: new[] { "Source", "Timestamp" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Telemetry");
+        }
+    }
+}
